47abb2e35760009d29ca8f50ea2ffe2c20320fea
dante-biase <45440995+dante-biase@users.noreply.github.com>
2021-12-09 21:30:38 -0500

    implemented no wait features

--- exportal.py
+++ exportal.py
@@ -7,23 +7,20 @@ class ExecutionPortal:
         self._s2a_queue = asyncio.Queue()
         self._a2s_queue = asyncio.Queue()
 
-    def pass_to_async(self, item):
-        asyncio.run_coroutine_threadsafe(self._s2a_queue.put(item), self._loop).result()
-
-    # def sync_put_nowait(self, item):
-    #     self._loop.call_soon(self._queue.put_nowait, item)
-
+    def pass_to_async(self, item, wait=True):
+        if wait:
+            asyncio.run_coroutine_threadsafe(self._s2a_queue.put(item), self._loop).result()
+        else:
+            self._loop.call_soon(self._s2a_queue.put_nowait, item)
+ 
     def get_from_async(self):
         return asyncio.run_coroutine_threadsafe(self._a2s_queue.get(), self._loop).result()
 
-    async def pass_to_sync(self, item):
-        await self._a2s_queue.put(item)
-
-    # def async_put_nowait(self, item):
-    #     self._queue.put_nowait(item)
+    async def pass_to_sync(self, item, wait=True):
+        if wait:
+            await self._a2s_queue.put(item)
+        else:
+            self._a2s_queue.put_nowait(item)
 
     async def get_from_sync(self):
         return await self._s2a_queue.get()
-
-
-
